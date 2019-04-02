171cb0b764adebd4fcb284bc70490ae552f497bf
svoboda18 <ruspam18@gmail.com>
2019-04-02 18:06:14 +0000

    x5: bluetooth: change bt name rootdir clean-up

---
+++ device/DOOGEE/X5/bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,13 @@
+/*
+  bluetooth/bdroid_buildcfg.h
+
+  add this file to the device tree
+  to change the bluetooth name on the user-space
+*/
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME "DOOGEE X5"
+
+#endif
