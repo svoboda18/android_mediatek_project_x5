5e1f731c79fda257016f3764639f30dd0e2e6ff6
SaMad SegMane <dev.svoboda18@gmail.com>
2020-03-07 15:11:22 +0100

    device: k80_bsp: Override PRODUCT_SHIPPING_API_LEVEL
    * required for getting ride of SAR, and get a successful build. 
    * override device information.

--- device/mediateksample/k80_bsp/full_k80_bsp.mk
+++ device/mediateksample/k80_bsp/full_k80_bsp.mk
@@ -1,6 +1,8 @@
 # include this too
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Oreo (mr1) shipped, make sure full treble is present.
+PRODUCT_SHIPPING_API_LEVEL_OVERRIDE := 27
 
 # Set target and base project for flavor build
 MTK_TARGET_PROJECT := $(subst full_,,$(TARGET_PRODUCT))
@@ -8,19 +10,19 @@ MTK_BASE_PROJECT := $(MTK_TARGET_PROJECT)
 MTK_PROJECT_FOLDER := $(LOCAL_PATH)
 MTK_TARGET_PROJECT_FOLDER := $(LOCAL_PATH)
 
-# This is where we'd set a backup provider if we had one
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
-
-# ProjectConfig
-include $(LOCAL_PATH)/ProjectConfig.mk
-
-# Set those variables here to overwrite the inherited values.
+# Set those variables here to be overwritten.
 PRODUCT_MANUFACTURER := DOOGEE
 PRODUCT_NAME := full_k80_bsp
 PRODUCT_DEVICE := k80_bsp
 PRODUCT_MODEL := X5
 PRODUCT_POLICY := android.policy_phone
 PRODUCT_BRAND := DOOGEE
+
+# This is where we'd set a backup provider if we had one
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# ProjectConfig
+include $(LOCAL_PATH)/ProjectConfig.mk
 
 BUILD_FINGERPRINT := "DOOGEE/full_hct6580_weg_c_m/hct6580_weg_c_m:6.0/MRA58K/1472705112:user/test-keys"
 PRODUCT_DEFAULT_LOCALE := en_US
