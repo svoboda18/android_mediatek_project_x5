9dd34cd96f99681aca8310fde7f241eb501b7c40
svoboda18 <dev.svoboda18@gmail.com>
2023-10-01 18:51:54 +0200

    system: connectivity: gps: ignore LBS app
    Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

---
+++ device/mediatek/system/common/connectivity/product_package/gps_product_package.mk
@@ -0,0 +1,12 @@
+# Always build LBS packages
+PRODUCT_PACKAGES += LocationEM2 \
+                  AutoDialer \
+                  LPPeService \
+                  NlpService
+#svb
+#NonFrameworkLbs
+
+# Mtk Nlp support
+# PRODUCT_PACKAGES += MtkNlp
+
+#PRODUCT_PACKAGES += lbs_dbg
